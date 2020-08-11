d4e628bb6c6faa307d2ebd84f9dcd09024462dc8
Oleg Grytsynevych <oleggry@amazon.com>
2020-08-12 00:46:04 +0200

    Refactored canvas.

--- views/canvas.js
+++ views/canvas.js
@@ -6,7 +6,11 @@
     const ctx = myCanvas.getContext('2d');
 
     const xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = processServerResponse;
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        processInferenceResult(this.responseText);
+      }
+    };
 
     const videoStream = await navigator.mediaDevices.getUserMedia({
       audio: false,
@@ -20,21 +24,19 @@
     async function predict() {
       ctx.drawImage(myVideo, 0, 0, 500, 500);
       await serverPredict(myCanvas);
-      
       //requestAnimationFrame(predict);
       setTimeout(() => predict(), 300);
     }
 
     async function serverPredict(cnv) {
       xhttp.open('POST', '/inference/api/canvas', true);
+      // TODO: send the canvas
       xhttp.send();
     }
 
-    function processServerResponse() {
-      if (this.readyState == 4 && this.status == 200) {
-        console.log(this.responseText);
-        //status.innerHTML = `Predictions: ${predictions[0].className}@${predictions[0].probability.toPrecision(1)}`;
-        //backend.innerHTML = `Backend: ${tf.getBackend()}`;
-      }
+    function processInferenceResult(result) {
+      console.log(result);
+      //status.innerHTML = `Predictions: ${predictions[0].className}@${predictions[0].probability.toPrecision(1)}`;
+      //backend.innerHTML = `Backend: ${tf.getBackend()}`;
     }
   })();
